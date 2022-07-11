86f19a3578e4399273330590f32224751fc0a543
PhongLuong99 <77917776+PhongLuong99@users.noreply.github.com>
2022-07-11 12:24:59 +0700

    TIME

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ server.use(jsonServer.bodyParser)
 server.use((req, res, next) => {
   if (req.method === 'POST') {
     req.body.createdAt = Date.now()
-    req.body.updatedAt = Date.parse()
+    req.body.updatedAt = Date.UTC(0, 0, 0, 0, 0, 0)
   }
   // Continue to JSON Server router
   next()
